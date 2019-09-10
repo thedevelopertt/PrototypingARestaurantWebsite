662f79729154d7a1b2d00d90af22cbfe3222c618
Greg <greigdillon@gmail.com>
2019-09-10 11:48:07 -0500

    Added more info section

--- index.php
+++ index.php
@@ -100,6 +100,134 @@
     </div>
 </section>
 
+<section class="info-section">
+    <div class="container-fluid">
+        <div class="info-row">
+
+            <div class="info-block">
+                <div class="info-block-row">
+                    <div class="info-title">
+                        <h3>AWARDS</h3>
+                    </div>
+
+                    <div class="info-large-title">
+                        <h2>QUAY NAMED HOTTEST RESTAURANT & DISH IN AUSTRALIA</h2>
+                    </div>
+
+                    <div class="info-lead">
+                        <p>Quay was named 2019 Hottest Restaurant and Hottest Dish in Australia by John Lethlean in the Weekend Australian Hot 50.</p>
+                    </div>
+
+                    <div class="info-action">
+                        <a href="#">VIEW STORY</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="info-block">
+                <div class="info-block-row">
+                    <div class="info-title">
+                        <h3>FESTIVE SEASON</h3>
+                    </div>
+
+                    <div class="info-large-title">
+                        <h2>THURSDAY LUNCH IN DECEMBER</h2>
+                    </div>
+
+                    <div class="info-lead">
+                        <p>Quay to open Thursday Lunch in December. To celebrate the festive season, Quay will open for Thursday lunch in December.</p>
+                    </div>
+
+                    <div class="info-action">
+                        <a href="#">ADD BOOKING</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="info-block">
+                <div class="info-block-row">
+                    <div class="info-title">
+                        <h3>SPECIAL EVENT</h3>
+                    </div>
+
+                    <div class="info-large-title">
+                        <h2>RARE & SPECIAL WHISKY DINNER</h2>
+                    </div>
+
+                    <div class="info-lead">
+                        <p>This spring Bennelong & Quay invite you to join a very special Whisky dinner hosted by Fink Creative Director, John</p>
+                    </div>
+
+                    <div class="info-action">
+                        <a href="#">BOOK SPECIAL</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="info-block">
+                <div class="info-block-row">
+                    <div class="info-title">
+                        <h3>SPECIAL EVENT</h3>
+                    </div>
+
+                    <div class="info-large-title">
+                        <h2>CHRISTMAS DAY</h2>
+                    </div>
+
+                    <div class="info-lead">
+                        <p>Celebrate Christmas Day with friends and family on Sydney Harbour at Quay. Guests will enjoy a glass of Champagne and</p>
+                    </div>
+
+                    <div class="info-action">
+                        <a href="#">BOOK CHRISTMAS</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="info-block">
+                <div class="info-block-row">
+                    <div class="info-title">
+                        <h3>SPECIAL EVENT</h3>
+                    </div>
+
+                    <div class="info-large-title">
+                        <h2>NEW YEAR’S EVE</h2>
+                    </div>
+
+                    <div class="info-lead">
+                        <p>Celebrate a spectacular New Year’s Eve with absolute front row seats at Quay overlooking sparkling Sydney Harbour. Begin your evening</p>
+                    </div>
+
+                    <div class="info-action">
+                        <a href="#">BOOK NEW YEAR'S</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="info-block">
+                <div class="info-block-row">
+                    <div class="info-title">
+                        <h3>MORE INFO</h3>
+                    </div>
+
+                    <div class="info-large-title">
+                        <h2>EVENTS AND STORIES</h2>
+                    </div>
+
+                    <div class="info-lead">
+                        <p>Continue Reading our news.</p>
+                    </div>
+
+                    <div class="info-action">
+                        <a href="#">READ HERE</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
 
 </body>
 </html>
