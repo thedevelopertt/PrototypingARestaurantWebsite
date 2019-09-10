0cbfde6ae66d97e1e9c8bc0e40d1a7844515d26b
Greg <greigdillon@gmail.com>
2019-09-10 01:47:11 -0500

    Added about section Added scroll magic to node_modules

--- index.php
+++ index.php
@@ -43,9 +43,34 @@
                 </div>
 
                 <div class="food-menu-action">
-                    <a href="#">Read More</a>
+                    <a href="#">VIEW MENU</a>
                 </div>
             </div>
+        </div>
+    </div>
+</section>
+
+<section class="about-section">
+    <div class="container-fluid">
+        <div class="about-row">
+
+            <div class="about-image">
+            </div>
+
+            <div class="about-description">
+                <div>
+                    <h2>PETER GILMORE</h2>
+                </div>
+
+                <div>
+                    <p>Peter’s appreciation of nature’s diversity and his endless experimentation in the kitchen and garden are the driving forces in his cooking. Peter leads the Quay team with a focus on excellence, innovation and hospitality.</p>
+                </div>
+
+                <div class="about-action">
+                    <a href="#">READ MORE</a>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
